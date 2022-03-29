018d137dbf1328f4ede716b7f6c689540becb338
swag31415 <kidswagatam18@gmail.com>
2022-03-28 20:49:55 -0600

    Moved all the processing to a web worker

--- js/main.js
+++ js/main.js
@@ -1,39 +1,17 @@
-// -------------------------- COLOR METHODS -------------------------- //
-const rgb = (r, g, b, a = 255) => [r, g, b, a] // Create an rgba color
-const cmy = (c, m, y, k = 0) => [255 - c, 255 - m, 255 - y, 255 - k] // Create a cmyk color
-const hsv = (h, s, v, a = 1) => { // Create a hsv color
-  var r, g, b, i, f, p, q, t;
-  i = Math.floor(h * 6)
-  f = h * 6 - i
-  p = v * (1 - s)
-  q = v * (1 - f * s)
-  t = v * (1 - (1 - f) * s)
-  switch (i % 6) {
-    case 0: r = v, g = t, b = p; break
-    case 1: r = q, g = v, b = p; break
-    case 2: r = p, g = v, b = t; break
-    case 3: r = p, g = q, b = v; break
-    case 4: r = t, g = p, b = v; break
-    case 5: r = v, g = p, b = q; break
-  }
-  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255), Math.round(a * 255)]
+// DOM functions
+function display(img_data, w, h) {
+  // Put it on a canvas
+  let canvas = document.createElement("canvas")
+  canvas.width = w
+  canvas.height = h
+  canvas.getContext("2d").putImageData(img_data, 0, 0)
+  // Put the canvas on the screen
+  $("#artbox").html(`<img src="${canvas.toDataURL()}">`)
+  // Scroll to the art
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-const hex = (s) => { // Parse a hex string
-  m = s.match(/^#?([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})?$/i)
-  if (m) return [parseInt(m[1], 16), parseInt(m[2], 16), parseInt(m[3], 16), parseInt(m[4] || 'ff', 16)]
-  m = s.match(/^#?([0-9A-Fa-f])([0-9A-Fa-f])([0-9A-Fa-f])([0-9A-Fa-f])?$/i)
-  if (m) return [parseInt(m[1], 16)*0x11, parseInt(m[2], 16)*0x11, parseInt(m[3], 16)*0x11, parseInt(m[4] || 'f', 16)*0x11]
-}
-const eq = (a, b) => // To compare colors
-  a.length === b.length && a.every((v, i) => v === b[i]);
-const clamp = (n, low, high) => n > high ? high : (n < low ? low : n)
-// ------------------------- Image Loading --------------------------- //
-const arr = (dims, f) => {
-  let [n, ...others] = dims
-  return Array.from({length: n}, (_,i) => (
-    others.length > 0 ? arr(others, (...p) => f(i, ...p)) : f(i)
-  ))
-}
+
 const upload = () => new Promise(res => {
   let input = document.createElement('input')
   input.type = 'file'
@@ -47,8 +25,7 @@ const upload = () => new Promise(res => {
         ctx.canvas.width = img.width
         ctx.canvas.height = img.height
         ctx.drawImage(img, 0, 0, img.width, img.height)
-        let data = ctx.getImageData(0, 0, img.width, img.height)
-        res(arr([data.height, data.width, 4], (y, x, i) => data.data[(y*img.width*4) + (x*4) + i]))
+        res(ctx.getImageData(0, 0, img.width, img.height))
       }
       img.src = e.target.result
     }
@@ -56,58 +33,21 @@ const upload = () => new Promise(res => {
   }
   input.click()
 })
-// ------------------------------------------------------------------- //
 
-// This is silly hehe
-const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor
-async function update_image(code) {
-  // Get parameters
-  let w = $("#width").val()
-  let h = $("#height").val()
-  let n_runs = $("#n_runs").val()
+// Worker stuff
+var worker = new Worker('js/process.js')
 
-  // Create the image
-  var img = new Uint8ClampedArray(4 * w * h)
+worker.addEventListener('message', function(e) {
+  let [task, ...rest] = e.data
+  if (task == 'display') display(...rest)
+  else if (task == 'upload') upload().then(d => worker.postMessage(['upload-done', d]))
+}, false)
 
-  // Initialize access methods
-  let calc = (x, y) => 4 * (y * w + x)
-  let get = (x, y) => {
-    let idx = calc(clamp(x, 0, w), clamp(y, 0, h))
-    return [img[idx], img[idx + 1], img[idx + 2], img[idx + 3]]
-  }
+worker.addEventListener('error', function (e) {
+  console.error('ERROR: Line ', e.lineno, ' in ', e.filename, ': ', e.message)
+}, false)
 
-  // The user's code
-  let func = AsyncFunction('x', 'y', 'i', 'w', 'h', 'get', 'rel', code)
-
-  for (let i = 0; i < n_runs; i++) {
-    for (let y = 0; y < h; y++) {
-      for (let x = 0; x < w; x++) {
-        // Run the user's function and calculate
-        let pix = await func(x, y, i, w, h, get, (dx, dy) => get(x+dx, y+dy))
-        // Assign the pixel values
-        let idx = calc(x, y)
-        img[idx + 0] = pix[0]
-        img[idx + 1] = pix[1]
-        img[idx + 2] = pix[2]
-        img[idx + 3] = pix[3]
-      }
-    }
-  }
-
-  let img_data = new ImageData(img, w, h)
-  let canvas = document.createElement("canvas")
-  canvas.width = w
-  canvas.height = h
-  canvas.getContext("2d").putImageData(img_data, 0, 0)
-
-  $("#artbox").html(`<img src="${canvas.toDataURL()}">`)
-}
-
-function scroll_to_top() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
+// User interactions
 // TODO: Improve this mess
 $("textarea.code").each((i, v) => {
   if (v.value.split('\n').length < 2) v.value += "\n\n"
@@ -127,7 +67,7 @@ $("textarea.code").each((i, v) => {
     .text(v.id == "editor" ? "Run" : "Try it!")
     .appendTo(cm.display.wrapper)
     .click(() => {
-      update_image(cm.getValue())
-      scroll_to_top()
+      // Send it to the worker
+      worker.postMessage(['run', cm.getValue(), $("#width").val(), $("#height").val(), $("#n_runs").val()])
     })
 })
---
+++ js/process.js
@@ -0,0 +1,83 @@
+// -------------------------- COLOR METHODS -------------------------- //
+const rgb = (r, g, b, a = 255) => [r, g, b, a] // Create an rgba color
+const cmy = (c, m, y, k = 0) => [255 - c, 255 - m, 255 - y, 255 - k] // Create a cmyk color
+const hsv = (h, s, v, a = 1) => { // Create a hsv color
+  var r, g, b, i, f, p, q, t;
+  i = Math.floor(h * 6)
+  f = h * 6 - i
+  p = v * (1 - s)
+  q = v * (1 - f * s)
+  t = v * (1 - (1 - f) * s)
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break
+    case 1: r = q, g = v, b = p; break
+    case 2: r = p, g = v, b = t; break
+    case 3: r = p, g = q, b = v; break
+    case 4: r = t, g = p, b = v; break
+    case 5: r = v, g = p, b = q; break
+  }
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255), Math.round(a * 255)]
+}
+const hex = (s) => { // Parse a hex string
+  m = s.match(/^#?([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})?$/i)
+  if (m) return [parseInt(m[1], 16), parseInt(m[2], 16), parseInt(m[3], 16), parseInt(m[4] || 'ff', 16)]
+  m = s.match(/^#?([0-9A-Fa-f])([0-9A-Fa-f])([0-9A-Fa-f])([0-9A-Fa-f])?$/i)
+  if (m) return [parseInt(m[1], 16)*0x11, parseInt(m[2], 16)*0x11, parseInt(m[3], 16)*0x11, parseInt(m[4] || 'f', 16)*0x11]
+}
+const eq = (a, b) => // To compare colors
+  a.length === b.length && a.every((v, i) => v === b[i]);
+const clamp = (n, low, high) => n > high ? high : (n < low ? low : n)
+// ------------------------- Image Loading --------------------------- //
+const arr = (dims, f) => {
+  let [n, ...others] = dims
+  return Array.from({length: n}, (_,i) => (
+    others.length > 0 ? arr(others, (...p) => f(i, ...p)) : f(i)
+  ))
+}
+var uploading = null
+const upload = async () => {
+  let data = await new Promise(res => {
+    self.postMessage(['upload'])
+    uploading = res
+  })
+  data = data[0]
+  return arr([data.height, data.width, 4], (y, x, i) => data.data[(y*data.width*4) + (x*4) + i])
+}
+// ------------------------------------------------------------------- //
+
+// This is silly hehe
+const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor
+async function update_image(code, w, h, n_runs) {
+  // Create the image
+  var img = new Uint8ClampedArray(4 * w * h)
+  // Initialize access methods
+  let calc = (x, y) => 4 * (y * w + x)
+  let get = (x, y) => {
+    let idx = calc(clamp(x, 0, w), clamp(y, 0, h))
+    return [img[idx], img[idx + 1], img[idx + 2], img[idx + 3]]
+  }
+  // The user's code
+  let func = AsyncFunction('x', 'y', 'i', 'w', 'h', 'get', 'rel', code)
+  // The Mega Loop
+  for (let i = 0; i < n_runs; i++) {
+    for (let y = 0; y < h; y++) {
+      for (let x = 0; x < w; x++) {
+        // Run the user's function and calculate
+        let pix = await func(x, y, i, w, h, get, (dx, dy) => get(x+dx, y+dy))
+        // Assign the pixel values
+        let idx = calc(x, y)
+        img[idx + 0] = pix[0]
+        img[idx + 1] = pix[1]
+        img[idx + 2] = pix[2]
+        img[idx + 3] = pix[3]
+      }
+    }
+  }
+  return [new ImageData(img, w, h), w, h]
+}
+
+self.addEventListener('message', function(e) {
+  let [task, ...rest] = e.data
+  if (task == 'run') update_image(...rest).then(d => self.postMessage(['display', ...d]))
+  else if (task == 'upload-done') uploading(rest)
+}, false)
